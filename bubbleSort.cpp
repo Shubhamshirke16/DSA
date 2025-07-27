b40e3caf2a8d09ed7f49b34f20f8d5968f47fad7
Shubham Shirke <105706685+Shubhamshirke16@users.noreply.github.com>
2025-07-27 19:58:46 +0530

    Create bubbleSort.cpp

---
+++ bubbleSort.cpp
@@ -0,0 +1,34 @@
+//bubbleSort
+
+#include <iostream>
+using namespace std;
+#include<vector>
+#include <algorithm>
+
+
+
+void bubbleSort(vector<int>& arr, int n)
+{   
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=0; j<n-i-1; j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                swap(arr[j], arr[j+1]);
+            }
+        }
+    }
+}
+
+int main() 
+{
+    int n=6;
+    vector<int> arr = {0,3, 4, 7, 10, 9};
+    bubbleSort(arr, n);
+    for(int i=0;i<n;i++)
+    {
+      cout <<arr[i]<<endl;
+    }
+    return 0;
+}
