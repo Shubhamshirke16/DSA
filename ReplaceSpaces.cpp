5a4a5ae52289d07a37ddab6c0fe0effbd5732699
Shubham Shirke <105706685+Shubhamshirke16@users.noreply.github.com>
2025-07-30 09:40:03 +0530

    Create ReplaceSpaces.cpp

---
+++ ReplaceSpaces.cpp
@@ -0,0 +1,21 @@
+//Replace Spaces
+
+#include <bits/stdc++.h> 
+string replaceSpaces(string &str){
+	string temp="";
+	int count=0;
+	for(int i=0;i<str.length();i++)
+	{
+		if(str[i]==' ')
+		{
+			temp.push_back('@');
+			temp.push_back('4');
+			temp.push_back('0');
+		}
+		else
+		{
+			temp.push_back(str[i]);
+		}
+	}
+	return temp;
+}
