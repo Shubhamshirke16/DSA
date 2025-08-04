3205c3f5cfa6495a9541f5e2342ca1d257d3ead6
Shubham Shirke <105706685+Shubhamshirke16@users.noreply.github.com>
2025-08-04 10:16:01 +0530

    Create sayDigits71.cpp

---
+++ sayDigits71.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void sayDigits(int n,string arr[]){
+  if(n==0)
+  {
+    return ;
+  }
+  
+  int digit=n%10;
+  n=n/10;
+  sayDigits(n,arr);
+  cout<<arr[digit]<<" ";
+}
+
+int main() 
+{
+    int n=25599566,res;
+    string arr[]={"zero","one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    sayDigits(n,arr);
+    return 0;
+}
